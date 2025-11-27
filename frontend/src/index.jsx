0a1d8d83a8ca442261069fd31246f56b0e107e97
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-27 20:34:20 +0300

    Delete frontend/src/index.jsx

--- frontend/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './assets/index.css';
-import './assets/customCss.css';
-import Init from './init';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  Init(),
-);
