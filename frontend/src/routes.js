91565adab9a186326b91495ba952910e2eb506ee
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-27 20:33:57 +0300

    Update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,8 @@
-// const host = 'http://0.0.0.0:5001'; // for local tests
-export default {
-  login: () => ['api', 'v1', 'login'].join('/'),
-  getData: () => ['api', 'v1', 'data'].join('/'),
-  creatNewUser: () => ['api', 'v1', 'signup'].join('/'),
-  logIn: '/login',
-  signup: '/signup',
-  err: '*',
-  main: '/',
-};
+const apiPath = '/api/v1'
 
-// const host = 'http://localhost:8080'; // for local tests
-// export default {
-//   tasksPath: () => [host, 'tasks'].join('/'),
-//   taskPath: (id) => [host, 'tasks', id].join('/'),
-//   finishTaskPath: (id) => [host, 'tasks', id, 'finish'].join('/'),
-//   activateTaskPath: (id) => [host, 'tasks', id, 'activate'].join('/'),
-// };
+export default {
+  signupPath: () => [apiPath, 'signup'].join('/'),
+  loginPath: () => [apiPath, 'login'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
+  addMessagePath: () => [apiPath, 'messages'].join('/'),
+}
