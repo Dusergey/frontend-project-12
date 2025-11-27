67591f1839da736027229498dfb66f0884baac85
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-11-27 20:33:21 +0300

    Delete frontend/src/hooks directory

--- frontend/src/hooks/apiHook.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import ApiContext from '../context/ApiContext';
-
-const useApi = () => useContext(ApiContext);
-
-export default useApi;
--- frontend/src/hooks/authHooks.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import authContext from '../context/AuthContext';
-
-const useAuth = () => useContext(authContext);
-
-export default useAuth;
