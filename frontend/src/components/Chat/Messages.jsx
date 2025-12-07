9928b98ee0ab04a1d0907d063de4bd696fcf4bba
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-12-07 13:31:08 +0300

    Update Messages.jsx

--- frontend/src/components/Chat/Messages.jsx
+++ frontend/src/components/Chat/Messages.jsx
@@ -8,9 +8,19 @@ const Messages = ({ channels, currentChannelId, currentChannelChat, inputValue, 
 
   const autoScrollRef = useRef()
 
+  //  добавляем ref для инпута
+  const inputRef = useRef(null)
+
   useEffect(() => {
     autoScrollRef.current.scrollIntoView()
   }, [currentChannelChat])
+
+  //  автофокус при смене канала
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [currentChannelId])
 
   return (
     <Col className="p-0 h-100">
@@ -19,7 +29,9 @@ const Messages = ({ channels, currentChannelId, currentChannelChat, inputValue, 
           <p className="m-0">
             <b>{`# ${channels.find(c => c.id === currentChannelId)?.name}`}</b>
           </p>
-          <span className="text-muted">{t('messages.messagesCounter.messagesCount', { count: currentChannelChat.length })}</span>
+          <span className="text-muted">
+            {t('messages.messagesCounter.messagesCount', { count: currentChannelChat.length })}
+          </span>
         </div>
 
         <div id="messages-box" className="chat-messages overflow-auto px-5">
@@ -36,6 +48,7 @@ const Messages = ({ channels, currentChannelId, currentChannelChat, inputValue, 
           <Form className="py-1 border rounded-2" onSubmit={handleSubmit}>
             <div className="input-group has-validation">
               <Form.Control
+                ref={inputRef} // 👈 добавили ссылку
                 name="body"
                 type="text"
                 aria-label={t('chat.ariaLabel')}
@@ -44,7 +57,11 @@ const Messages = ({ channels, currentChannelId, currentChannelChat, inputValue, 
                 value={inputValue}
                 onChange={handleChange}
               />
-              <Button type="submit" variant="light" className={`btn-group-vertical ${inputValue.trim() ? '' : 'disabled'}`}>
+              <Button
+                type="submit"
+                variant="light"
+                className={`btn-group-vertical ${inputValue.trim() ? '' : 'disabled'}`}
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor" className="bi bi-arrow-right-square">
                   <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
                 </svg>
